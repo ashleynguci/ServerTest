d79d4257d7d01234baa61596a197a5013a128d08
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-02-09 21:36:16 +0200

    Add CRUD functionalities to Bookstore app

--- bookStore_/src/main/java/fi/haagahelia/bookStore_/web/BookController.java
+++ bookStore_/src/main/java/fi/haagahelia/bookStore_/web/BookController.java
@@ -3,8 +3,11 @@ package fi.haagahelia.bookStore_.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.bookStore_.domain.Book;
 import fi.haagahelia.bookStore_.domain.BookRepository;
 
 @Controller
@@ -15,5 +18,27 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books",repository.findAll());
 		return "booklist";
+	}
+	@RequestMapping(value="/add")
+	public String addBook(Model model) {
+		model.addAttribute("book",new Book());
+		return "addBook";
+	}
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	@RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
+	public String deleteBook(@PathVariable("id")Long bookId, Model model ) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	@RequestMapping(value = "/edit/{id}")
+	public String edit(@PathVariable("id") Long bookId, Model model){
+	model.addAttribute("book", repository.findById(bookId));
+	
+	return "editbook";
+	
 	}
 }
